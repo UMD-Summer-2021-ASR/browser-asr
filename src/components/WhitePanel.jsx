74971de91eb2a4749a063005abaea4215333b9e7
Andrew Chen <andrew.lang.chen@gmail.com>
2021-06-19 15:02:36 -0400

    login panel kind of done

--- src/components/WhitePanel.jsx
+++ src/components/WhitePanel.jsx
@@ -1,13 +1,34 @@
 import "../styles/WhitePanel.css";
+import CheckIcon from '@material-ui/icons/Check';
+import GoogleLogin from 'react-google-login';
+
+function LoginCardItem(props) {
+    return (
+        <div class="login-card-item">
+            <CheckIcon style={{ color:"green" }} fontSize="small"/>
+            <div class="mr-1"></div>
+            {props.text}
+        </div>
+    );
+}
 
 function MainBody() {
     return (
         <div class="main-body">
             <div class="login-card">
-            <div class="card-title">LOGIN</div>
-        </div>
+                <div class="card-title" id="login-title">Login</div>
+                <br/>
+                <LoginCardItem text="Unlimited games"/>
+                <LoginCardItem text="Singleplayer mode"/>
+                <LoginCardItem text="Party mode"/>
+                <LoginCardItem text="Play with friends"/>
+                <LoginCardItem text="Contribute to ASR research"/>
+                <div class="google-login-wrapper">
+                    <GoogleLogin theme="light"/>
+                </div>
+            </div>
             <div class="gamemode-card">
-                <div class="card-title">GAMEMODES</div>
+                <div class="card-title">Gamemodes</div>
             </div>
         </div>
     );
