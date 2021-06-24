c8d1c8b1572af3535574c39795e8cc0de737d90e
Andrew Chen <andrew.lang.chen@gmail.com>
2021-06-24 00:21:44 -0400

    vtt reading

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import useSound from "use-sound";
-import testAudio from "../assets/obama.wav";
-
+import VTT from "../assets/id01.vtt";
 
 class Player extends React.Component {
     constructor(props) {
         super(props);
         this.state = {gameState: 0}; // 0 = begin, 1 = question is running, 2 = question is done
+        this.transcript = [];
+
+        this.loadNextTranscript();
     }
 
     buzz() {
@@ -27,10 +29,21 @@ class Player extends React.Component {
         return true;
     }
 
+    loadNextTranscript() {
+        const webvtt = require('node-webvtt');
+ 
+        const parsed = webvtt.parse(VTT);
+        //write catch for parse fail
+        console.log(parsed);
+        // this.transcript.push(new Array());
+    }
+
     render() {
         return (
             <div>
-                
+                <button onClick={this.loadNextTranscript}>
+                    Hi
+                </button>
             </div>
         );
     }
