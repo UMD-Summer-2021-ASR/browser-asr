3ef7be70eb206c8261ad48141da39dc4e1b67839
Andrew Chen <andrew.lang.chen@gmail.com>
2021-07-01 09:59:59 -0400

    refactored

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import useSound from "use-sound";
-import VTT from "../assets/id01.vtt";
-import WavAudio from '../assets/id01.wav';
+import VTT1 from "../assets/id01.vtt";
+import VTT2 from "../assets/quizzr_q1.vtt";
+import VTT3 from "../assets/quizzr_q2.vtt";
+import VTT4 from "../assets/quizzr_q3.vtt";
+import VTT5 from "../assets/quizzr_q4.vtt";
+import VTT6 from "../assets/quizzr_q5.vtt";
+import wav1 from '../assets/id01.wav';
+import wav2 from '../assets/quizzr_q1.wav';
+import wav3 from '../assets/quizzr_q2.wav';
+import wav4 from '../assets/quizzr_q3.wav';
+import wav5 from '../assets/quizzr_q4.wav';
+import wav6 from '../assets/quizzr_q5.wav';
+
 import "../styles/Player.css";
 
 function PlayButton(props) {
@@ -27,8 +38,11 @@ class Player extends React.Component {
         super(props);
         this.state = {gameState: 0, transcriptState: ""}; // 0 = begin, 1 = question is running, 2 = question is done
         this.transcript = []; // [i][0] = ms from start, [i][1] = word
-
-        this.loadNextTranscript();
+        this.currentAudio = new Audio();
+        this.VTTs = [VTT1, VTT2, VTT3, VTT4, VTT5, VTT6];
+        this.Audios = [wav1, wav2, wav3, wav4, wav5, wav6];
+        this.currentQuestion = 0;
+        this.timeOuts = [];
     }
 
     buzz() {
@@ -49,9 +63,9 @@ class Player extends React.Component {
         })
         for(let i = 0; i < transcript.length; i++) {
             console.log(transcript[i][1] + transcript[i][0]);
-            setTimeout(() => {this.setState({
+            this.timeOuts.push(setTimeout(() => {this.setState({
                 transcriptState: this.state.transcriptState + transcript[i][1] + " "
-            })}, transcript[i][0]);
+            })}, transcript[i][0]));
         }
     }
 
@@ -60,12 +74,18 @@ class Player extends React.Component {
         return true;
     }
 
-    loadNextTranscript() {
-        fetch(VTT)
+    loadNextTranscript(VTTFile) {
+        // GET VTT
+        this.transcript = [];
+        return fetch(VTTFile)
             .then(response => response.text())
             .then(text => {
                 let lines = text.split("\n");
                 for(let i = 2; i < lines.length-1; i+=3) {
+                    if(lines[i+1].charAt(0) === "<") {
+                        let lineSplit = lines[i+1].split(">");
+                        lines[i+1] = lineSplit[1];
+                    }
                     let startTime = lines[i].split(" ")[0];
                     let minutes = parseInt(startTime.split(":")[0]);
                     let seconds = parseFloat(startTime.split(":")[1]) + minutes*60;
@@ -73,6 +93,20 @@ class Player extends React.Component {
                     this.transcript.push(new Array(ms, lines[i+1]));
                 }
             });
+    }
+
+    loadNextAudio(audioFile) {
+        this.currentAudio = new Audio(audioFile);
+    }
+
+    loadNextQuestion() {
+        this.loadNextTranscript(this.VTTs[this.currentQuestion]).then(() => {
+            this.loadNextAudio(this.Audios[this.currentQuestion]);
+            this.currentQuestion++;
+            if(this.currentQuestion >= 6) {
+                this.currentQuestion = 0;
+            }
+        });
     }
 
     render() {
@@ -83,14 +117,32 @@ class Player extends React.Component {
                     {this.state.transcriptState}
                 </div>
                 
-                <a class="play-btn" onClick={() => {
-                    this.playTranscript(this.transcript);
-                    let x = new Audio(WavAudio);
-                    x.play();
-                }}>
-                    Play
-                </a>
-                
+                <div class="player-button-wrapper">
+                    <a class="play-btn" onClick={() => {
+                        this.playTranscript(this.transcript);
+                        console.log(this.transcript);
+                        this.currentAudio.play();
+                        
+                    }}>
+                        Play
+                    </a>
+
+                    <a class="play-btn" onClick={() => {
+                        this.currentAudio.pause();
+                        for (let i = 0; i < this.timeOuts.length; i++) {
+                            clearTimeout(this.timeOuts[i]);
+                        }
+                        this.timeOuts = [];
+
+                        this.setState({
+                            transcriptState: ""
+                        });
+                        
+                        this.loadNextQuestion();
+                    }}>
+                        Load Next
+                    </a>
+                </div>
             </div>
         );
     }
